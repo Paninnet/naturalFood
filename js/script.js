bdc9f6c39031c9ddf9a7b20be0e08e3b54fdf8e1
PaninNet <iliapanin300999@mail.ru>
2021-05-30 19:06:50 +0300

    Slider part 1

--- js/script.js
+++ js/script.js
@@ -298,9 +298,82 @@ window.addEventListener("DOMContentLoaded", () => {
 
 
 
-  fetch('http://localhost:3000/menu')
-  .then(data =>data.json())
-  .then(res =>console.log(res))
+  // Slider
+
+  let prevSlide = document.querySelector('.offer__slider-prev')
+  let nextSlide = document.querySelector('.offer__slider-next')
+
+  let currentSlide = document.querySelector('#current')
+  let totalSlide = document.querySelector("#total")
+   
+  const allImg = ["img/slider/pepper.jpg","img/slider/food-12.jpg","img/slider/olive-oil.jpg","img/slider/paprika.jpg",]
+ 
+  let offer__img = document.querySelector('.offer__img')
+  
+  function showTotalSlide(array) {
+    let totalAllImg=0
+    for (let i =0; i<array.length; i++){
+      totalAllImg++
+    }
+    console.log(totalAllImg);
+    if (totalAllImg <10){
+      totalSlide.innerHTML=`0${totalAllImg}`
+    }
+    else{
+      totalSlide.innerHTML=totalAllImg
+    }
+    
+  }
+
+  let currentIndex = 1
+  function showCurrentSlide() {
+    if(currentIndex < 10){
+      currentSlide.innerHTML=`0${currentIndex}`
+    }
+    else{
+      currentSlide.innerHTML= currentIndex
+    }
+    
+  }
+
+  
+    nextSlide.addEventListener('click',()=>{
+      if(currentIndex >= allImg.length){
+        currentIndex=0
+        currentIndex++
+        offer__img.src=allImg[currentIndex-1]
+        showCurrentSlide(allImg)
+      }else{
+        currentIndex++
+        offer__img.src=allImg[currentIndex-1]
+        showCurrentSlide(allImg)
+      }
+      
+      console.log(currentIndex);
+    })
+  
+
+ 
+    prevSlide.addEventListener('click',()=>{
+      
+      if(currentIndex <= 1){
+        currentIndex=allImg.length+1
+        currentIndex--
+        offer__img.src=allImg[currentIndex-1]
+        showCurrentSlide(allImg)
+      }
+      else{
+        currentIndex--
+        offer__img.src=allImg[currentIndex-1]
+        showCurrentSlide(allImg)
+      }
+      console.log(currentIndex);
+    })
+  
+
+  showCurrentSlide()
+  showTotalSlide(allImg)
+
 })
 
 
